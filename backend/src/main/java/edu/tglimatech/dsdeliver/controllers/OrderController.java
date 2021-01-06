b257bdbff5fab4bca8e49c93404754b9dc01229f
tglima <thg.limadesousa@gmail.com>
2021-01-05 22:26:34 -0300

    PUT /orders/{id}/delivered

--- backend/src/main/java/edu/tglimatech/dsdeliver/controllers/OrderController.java
+++ backend/src/main/java/edu/tglimatech/dsdeliver/controllers/OrderController.java
@@ -34,4 +34,11 @@ public class OrderController
                     .path("/{id}").buildAndExpand(dto.getId()).toUri();
         return ResponseEntity.created(uri).body(dto);
     }
+
+    @PutMapping("/{id}/delivered")
+    public ResponseEntity<OrderDTO> setDelivered(@PathVariable Long id)
+    {
+        OrderDTO dto = service.setDelivered(id);
+        return ResponseEntity.ok().body(dto);
+    }
 }
